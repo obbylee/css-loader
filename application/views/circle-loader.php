12b1ef01d41c6fd0430674a93fabedc9050dc145
Lee Obby <leeobbyw@gmail.com>
2021-01-04 15:23:12 +0700

    fetch

---
+++ application/views/circle-loader.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/css-loader.css">
+    <!-- <link rel="stylesheet" href="<?php echo base_url()?>assets/css/circle-loader.css"> -->
+    <!-- <link rel="stylesheet" href="<?php echo base_url()?>assets/css/text-loader.css"> -->
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bounce-loader.css">
+    <title>CSS Loaders</title>
+</head>
+<body>
+    <!-- Loader 1 -->
+    <!-- <div class="loader">
+        <div class="circle"></div>
+        <div class="circle"></div>
+    </div> -->
+
+    <!-- Loader 2 -->
+    <!-- <div class="loader">Loading</div> -->
+
+    <!-- Loader 3 -->
+    <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <section class="main">
+        <h1>Welcome To My Site</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur 
+            adipiscing elit. Nullam blandit venenatis 
+            mauris ut malesuada. Curabitur sit amet ornare lacus. 
+            Sed eu ornare ligula, non aliquam purus.
+             Morbi in sem vel ligula sollicitudin sodales eu luctus 
+             ante. Phasellus quis sem gravida tellus imperdiet hendrerit. Nam gravida scelerisque mauris, in volutpat sem mollis ut. Nulla purus tortor, gravida ac magna pharetra, varius placerat elit.</p>
+        <a href="#" class="btn">Read More</a>
+    </section>
+
+    <script src="<?php echo base_url()?>assets/js/main.js"></script>
+</body>
+</html>
